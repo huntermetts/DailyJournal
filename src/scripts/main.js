87ceb8ba07be8e80f7ad2052c4256d53fad77d93
Hunter Metts <huntermettsdev@gmail.com>
2018-12-17 11:40:33 -0600

    button console.logs

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,7 @@
 import journalForm from "./journalForm"
+import eventListener from "./eventListener"
 
 journalForm.formBuilder();
+
+let journalSaveButton = document.getElementById("journal_save")
+journalSaveButton.addEventListener("click", eventListener.handleJournalSave)
